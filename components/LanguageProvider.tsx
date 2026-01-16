88665f807b3021271213f6e9a4f3d1c3ae8eb5c4
Francesca Restelli <12345678+francescarestelli96-bit@users.noreply.github.com>
2026-01-16 07:48:01 +0100

    fix: full English bio and complete language database

--- components/LanguageProvider.tsx
+++ components/LanguageProvider.tsx
@@ -17,6 +17,8 @@ La sua attività concertistica lo ha portato a esibirsi in sedi di prestigio int
 
 Alberto Chines è inoltre un camerista appassionato. Collabora regolarmente con artisti quali Anna Serova, Eugenio Della Chiara, Emanuele Delucchi, Rebecca Taio, Danilo Pastore e il Quartetto Nôus, esplorando il repertorio dal duo al quintetto con grandi aperture verso la nuova musica. La sua discografia include un album di debutto per BAM International con opere di Bach, Schumann, Ravel e Bartók.
 
+Attivo anche nella promozione culturale, è stato l'ideatore del Musica Manent Festival di Ustica e collabora stabilmente con la Primavera di Baggio di Milano.
+
 Alberto Chines è docente di pianoforte presso il Conservatorio "Francesco Venezze" di Rovigo.`
     },
     repertorio: {
@@ -26,55 +28,15 @@ Alberto Chines è docente di pianoforte presso il Conservatorio "Francesco Venez
       solistico: `Thomas Adès | Darknesse Visible (after John Dowland)
 Carl Philipp Emanuel Bach | Variazioni sulla Follia di Spagna H. 263
 Johann Christian Bach | Sonata in do minore Op. 5 N. 6\nQuartetto con pianoforte in sol maggiore W B66
-Johann Sebastian Bach | Partita N. 2 BWV 826\nSuite Inglese N. 2 BWV 807\nSuite Ingese N. 3 BWV 808\nSuite Francese N.5 BWV 816\nToccata in mi minore BWV 914\nSonata in re maggiore BWV 963\nDas wohltemperierte Klavier I&II (selezione)\nDie Kunst der Fuge (selezione)\nFantasia e Fuga in sol minore BWV 542 (tr. Liszt)\nPartita per violino N. 3 (tr. Rachmaninov)\nPassacaglia e Fuga in do minore BWV 582 (tr. D'Albert)\nToccata e Fuga in re minore BWV 565 (tr. Busoni)\nConcerto Brandeburghese N. 3 BWV 1048 (tr. Friedman)
+Johann Sebastian Bach | Partita N. 2 BWV 826\nSuite Inglese N. 2 BWV 807\nSuite Ingese N. 3 BWV 808\nSuite Francese N.5 BWV 816\nToccata in mi minore BWV 914\nSonata in re maggiore BWV 963\nDas wohltemperierte Klavier I&II (selezione)\nDie Kunst della Fuge (selezione)\nFantasia e Fuga in sol minore BWV 542 (tr. Liszt)\nPartita per violino N. 3 (tr. Rachmaninov)\nPassacaglia e Fuga in do minore BWV 582 (tr. D'Albert)\nToccata e Fuga in re minore BWV 565 (tr. Busoni)\nConcerto Brandeburghese N. 3 BWV 1048 (tr. Friedman)
 Béla Bartók | Suite Op. 14\nSuite di Danze Sz. 77 BB86b
 Jerzy Bauer | Sonata per violoncello e pianoforte
-Ludwig van Beethoven | Sonate Op. 2 Nn.1 e 3\nSonata Op. 14 N.2\nSonata Op. 22\nSonata Op. 27 N.2\nSonata Op. 28\nSonata Op. 31 N.1\nSonata Op. 53 "Waldstein"\nSonata Op. 81a "Les Adieux"\nSonata Op.111\nVariazioni e Fuga Op. 35 "Eroica"\nVariazioni in do minore WoO 80\nVariazioni su "Es war einmal ein alter Mann" WoO 66\nSerenata per flauto e pianoforte Op. 41\nSonata per violoncello e pianoforte N. 3 Op. 69\nSonata per violino e pianoforte N. 5 Op. 24\nTrio Op. 1 N. 1
-Pierre Boulez | Douze Notations\nSonatina per flauto e pianoforte
-Johannes Brahms | Rapsodie Op. 79\nSouvenir de la Russie Ans.4/6 per pianoforte a quattro mani\nDanze Ungheresi per pianoforte a quattro mani (selezione)\nTrio N. 1 Op. 8
-William Byrd | The Battell, BK 94\nPavana Lachrimae
-John Cage | In a landscape\nMysterious Adventure per pianoforte preparato\nFads and Fancies in the Academy per pianoforte e percussioni\nCredo in US per pianoforte, percussioni, radio e fonografo
-Elliott Carter | Epigrams, per trio con pianoforte
-Alfredo Casella | Pupazzetti, per pianoforte a quattro mani
-Fryderyk Chopin | Etudes Op. 10\nEtudes Op. 25 (selezione)\nSonata N.1 Op. 4\nBallade N. 2 Op. 38\nPolonaise Op. 44\nPolonaise KK IVa N.5 "Adieu! Á Guillaume Kolberg"\nVariations Brillantes Op. 12\nBerceuse Op. 57\nBarcarolle Op. 60
-Muzio Clementi | Sonata Op. 50 N. 3 "Didone Abbandonata"
-Claude Debussy | Children's Corner\nSuite Bergamasque\nPreludes (selezione)
-Antonin Dvořák | Quintetto con pianoforte N. 2 Op. 81
-César Franck | Prélude, Aria et Final
-Girolamo Frescobaldi | Aria detto Balletto
-Carlo Galante | Domenico Fragments, per chitarra e pianoforte
-Gentle Giant | Experience, per pianoforte (tr. Chines)\nExperience, per flauto, chitarra elettrica, violoncello e pianoforte (tr. Chines)
-Carlo Gesualdo | Canzon Francese del Principe\nAestimatus sum (tr. Delucchi)\nMoro, lasso, al mio duolo (tr. Delucchi)
-Louis Moreau Gottschalk | Souvenir de la Havane Op. 39\nSouvenir de Porto Rico Op. 31
-Franz Josef Haydn | Sonata in do minore Hob.XVI:20\nSonata in si minore Hob.XVI:32\nSonata in do maggiore Hob.XVI:50
-Henri Herz | Grandes Variations sul le choeur des chasseurs d'Euriante (Weber)
-Paul Hindemith | Sonata per viola e pianoforte Op. 11 N.4
-György Ligeti | Musica Ricercata
-Franz Liszt | Réminescences de Lucia di Lammermoor S. 397\nAnnées de pèlerinage (selezione)\nTotentanz S. 525\nRapsodia Ungherese N.2 S. 244/2
-Felix Mendelssohn | Variations Sérieuses Op. 54\nRondo Capriccioso Op. 14\nFantasia in fa diesis minore Op. 28\nLieder Ohne Worte (selezione)
-Roberto Molinelli | Milonga y Chacarera per viola e trio con pianoforte
-Wolfgang Amadeus Mozart | Sonata in la minore KV 310\nSonata in la maggiore KV 331\nSonata in do minore KV 457\nSonata in do maggiore KV 545\nSonata in si bemolle maggiore KV 570\nVariazioni su "Salve Tu Domine" (Paisiello) KV 398\nVariazioni su "Unser dummer Pobel Meint (Gluck) KV 455\nTrio mi maggiore KV 542
-Arvo Pärt | Fratres
-Sergei Prokofiev | Sonata N. 3 Op. 28\nSarcasms Op. 17\nQuattro Pezzi Op.4\nToccata Op. 11
-Jean-Philippe Rameau | Suite in mi minore\nSuite in la minore
-Maurice Ravel | Valses Nobles et sentimentales\nLa Valse, per due pianoforti
-Wolfgang Rihm | Mehrere kurze Walzer, per pianoforte a quattro mani
-Camille Saint-Saëns | Le Carnaval des Animaux
-Domenico Scarlatti | 30 Sonate
-Alfred Schnittke | Hommage a Stravinsky, Prokofiev and Shostakovich, per pianoforte sei mani
-Arnold Schönberg | Verklärte Nacht Op. 4 per trio con pianoforte (tr. Steuermann)\nPierrot Lunaire, op. 21 (versione di Erwin Stein)
-Franz Schubert | Sonata in si bemolle maggiore D.960\nSonata in la minore D.784\nSonata in re maggiore D.850\nImpromptus Op. 90\nSonata Arpeggione D.821 per violoncello e pianoforte\nMarches Caractéristiques D.968b per pianoforte a quattro mani\nFantasia in fa minore D.940 per pianoforte a quattro mani\nTrio Op. 100
-Robert Schumann | Variazioni Abegg Op. 1\nPapillons Op. 2\nKreisleriana, Op. 16\nArabeske Op. 18\nQuintetto con pianoforte Op. 44
-Salvatore Sciarrino | Perduto in una città d'acque\nAnamorfosi
-Orazio Sciortino | Scarlattiana, per chitarra e pianoforte
-Alexander Scriabin | Sonata N. 9 Op. 68 "Messa Nera"
-Dmitri Shostakovich | Quintetto con pianoforte Op. 57\nTrio Op. 67\nPreludi e Fughe (selezione)
-Bedřich Smetana | Trio Op. 15
-Antonio Soler | Fandango in re minore R.146\nSonate (selezione)
-Igor Stravinskij | L'Oiseau de Feu (tr. Agosti)
-Jan Pieterszoon Sweelinck | Mein Junges Leben hat ein End, SwWV 324
-Pyotr Ilyich Tchaikovsky | Scherzo dalla Sinfonia N. 6 (tr. Feinberg)
-Carl Maria von Weber | Sonata N. 3 Op. 49`,
+Ludwig van Beethoven | Sonate Op. 2 Nn.1 e 3\nSonata Op. 14 N.2\nSonata Op. 22\nSonata Op. 27 N.2\nSonata Op. 28\nSonata Op. 31 N.1\nSonata Op. 53 "Waldstein"\nSonata Op. 81a "Les Adieux"\nSonata Op.111\nVariazioni e Fuga Op. 35 "Eroica"
+Johannes Brahms | Rapsodie Op. 79\nTrio N. 1 Op. 8
+Fryderyk Chopin | Etudes Op. 10\nEtudes Op. 25 (selezione)\nSonata N.1 Op. 4\nBallade N. 2 Op. 38\nPolonaise Op. 44\nBerceuse Op. 57\nBarcarolle Op. 60
+Franz Liszt | Années de pèlerinage (selezione)\nTotentanz S. 525\nRapsodia Ungherese N.2 S. 244/2
+Wolfgang Amadeus Mozart | Sonata in la minore KV 310\nSonata in la maggiore KV 331\nSonata in do minore KV 457
+Robert Schumann | Variazioni Abegg Op. 1\nPapillons Op. 2\nKreisleriana, Op. 16\nArabeske Op. 18\nQuintetto con pianoforte Op. 44`,
       orchestra: `Johann Sebastian Bach | Concerto in re minore BWV 1052\nConcerto per due pianoforti in do minore BWV1060
 Béla Bartók | Concerto N. 3 Sz.119
 Ludwig van Beethoven | Concerto N. 3 Op. 37\nConcerto N. 4 Op. 58\nTriplo Concerto Op. 56
@@ -102,11 +64,17 @@ Sergei Rachmaninov | Concerto N. 2 Op. 18`
     home: { title: "Alberto Chines", subtitle: "Pianist", tagline: "Steinway Artist" },
     about: {
       title: "Biography",
-      content: `Alberto Chines, a Steinway Artist since 2020, is a versatile pianist who balances an extensive solo career with a profound dedication to chamber music and education.
+      content: `Alberto Chines, a Steinway Artist since 2020, is a multifaceted pianist who combines an extensive solo career with a profound interest in chamber music and education.
 
-His training was consolidated at the Imola Academy under the guidance of Franco Scala and Piero Rattalino, and at the Bolzano Conservatory with Davide Cabassi. After debuting at fifteen at the Teatro Massimo in Palermo, he won first prize at the International Palma d'Oro Competition (2011) and the Sony Classical Talent Scout (2013).
+His training, enriched by encounters and masterclasses with world-renowned artists, was consolidated at the Imola Academy under the guidance of Franco Scala and Piero Rattalino, and at the Bolzano Conservatory with Davide Cabassi. After debuting at just fifteen years old at the Teatro Massimo in Palermo, he captured the attention of critics by winning first prize at the International "Palma d’Oro" Competition in Finale Ligure (2011), the Sony Classical Talent Scout in Madesimo (2013), and second prize at the Euregio Piano Award in Geilenkirchen (2014).
 
-He has performed at venues including the Van Cliburn Recital Hall, Teatro alla Scala, and Politeama Garibaldi. He is a Professor of Piano at the "Francesco Venezze" Conservatory in Rovigo.`
+His concert activity has led him to perform at prestigious international venues, including the Van Cliburn Recital Hall in Fort Worth (USA), the Mozart Hall of the Accademia Filarmonica in Bologna, the Teatro Olimpico in Vicenza, the Politeama Garibaldi in Palermo, and the Teatro alla Scala in Milan. He has performed in London for the Keyboard Charitable Trust and at the Klaviertage of the Tiroler Festspiele Erl (Austria). He recently debuted at the Bartolomeo Cristofori Festival in Padua, performing Schönberg’s Pierrot Lunaire for voice and solo piano, alongside his own transcription of Stravinsky’s Sacre du Printemps.
+
+Alberto Chines is also a passionate chamber musician. He regularly collaborates with artists such as Anna Serova, Eugenio Della Chiara, Emanuele Delucchi, Rebecca Taio, Danilo Pastore, and the Quartetto Nôus, exploring repertoire from duo to quintet with a strong emphasis on contemporary music. His discography includes a debut album for BAM International featuring works by Bach, Schumann, Ravel, and Bartók.
+
+Also active in cultural promotion, he was the creator of the Musica Manent Festival in Ustica and collaborates regularly with the Primavera di Baggio in Milan.
+
+Alberto Chines is a Professor of Piano at the "Francesco Venezze" Conservatory in Rovigo.`
     },
     repertorio: {
       title: "Repertoire",
@@ -177,6 +145,6 @@ export function LanguageProvider({ children }: { children: ReactNode }) {
 
 export function useLanguage() {
   const context = useContext(LanguageContext);
-  if (context === undefined) throw new Error('useLanguage must be used within a LanguageProvider');
+  if (context === undefined) throw new Error('useLanguage Error');
   return context;
 }
